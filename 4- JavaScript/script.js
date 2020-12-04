8a0a3a9ca57c81a820a9bb517090058153aa057d
Enes Alper Balta <71825882+eabalta@users.noreply.github.com>
2020-12-05 00:11:07 +0300

    incomplete js notes

---
+++ 4- JavaScript/script.js
@@ -0,0 +1,48 @@
+//Degiskenler
+
+// var client = {
+//     name: 'Enes Alper',
+//     surname: 'Balta',
+//     number: 12345,
+//     order: 10,
+//     gender: 'man',
+//     adress: 'Izmir',
+//     hobbies: 'women'
+// }
+
+// var order1 = '100';
+// var order2 = '275';
+// var totalOrder = Number(order1) + Number(order2);
+// console.log(totalOrder);
+
+//Sayi tahmini
+// alert('Welcome the Fight Club!')
+// let chance = 10;
+// let mystery_number = Math.floor((Math.random() * 100) + 1);
+// while (chance > 0) {
+//     chance--;
+//     let answer = prompt('Guess a number between 1 and 100');
+//     if (answer > mystery_number) {
+//         alert(`Down!`);
+//         alert(`Chance : ${chance}`);
+
+//         continue;
+//     } else if (answer < mystery_number) {
+//         alert(`Up!`);
+//         alert(`Chance : ${chance}`);
+
+//         continue;
+//     } else if (answer == mystery_number) {
+//         alert(`You win number is ${mystery_number}`);
+//         break
+//     }
+//     if (answer == "q") {
+//         break;
+//     }
+// }
+
+
+//Window Object
+
+// const a = 15;
+// var a2 = 35;
